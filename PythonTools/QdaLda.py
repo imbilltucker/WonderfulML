aa921b3235698161a6c9c0cba811bb3df35eaa2e
Michael Szczepaniak <mszczepaniak@pobox.com>
2017-03-04 13:47:31 -0700

    add init qda and lda notes, neural net graphics

---
+++ PythonTools/QdaLda.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def normald(X, mu=None, sigma=None):
+    """ Returns the normal probability density for a D-dimensional vector X
+        X 
+    """
+    
+    
+    return 0
